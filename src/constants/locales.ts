5cd6de1603776ae27c9fd6df3de9e08bd8e1f543
glebfarmon <glebfarmon@gmail.com>
2024-03-16 08:53:02 +0100

    Removed unused constant

--- src/constants/locales.ts
+++ src/constants/locales.ts
@@ -1,7 +1,3 @@
-export const LOCALES_FULL: Record<string, string> = {
-	en: 'English',
-	pl: 'Polski'
-}
-export const LOCALES: string[] = Object.keys(LOCALES_FULL)
+export const LOCALES: string[] = ['en', 'pl']
 export const DEFAULT_LOCALE: string = LOCALES[0]
 export const LOCALE_PREFIX: 'always' | 'as-needed' | 'never' = 'as-needed'
