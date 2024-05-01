fd787c155bddaeb81bf78ca77024f935364e7e36
glebfarmon <glebfarmon@gmail.com>
2024-05-01 23:48:03 +0200

    Fixed search skeleton

--- src/modules/exercises/search/skeleton.tsx
+++ src/modules/exercises/search/skeleton.tsx
@@ -1,5 +1,9 @@
 import {Skeleton} from '@/components/ui/skeleton'
 
 export const SearchSkeleton = () => {
-	return <Skeleton className={'w-full'} />
+	return (
+		<div className={'flex w-full h-10 rounded-md border border-input items-center px-3'}>
+			<Skeleton className={'w-14 h-4'} />
+		</div>
+	)
 }
