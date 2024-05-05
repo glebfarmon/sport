3792ce4cfec7bcbfd801894c07c64f4b2a4276de
glebfarmon <glebfarmon@gmail.com>
2024-05-06 00:53:51 +0200

    Changed "Store provider" location

--- src/components/providers/store-provider.tsx
+++ src/components/providers/store-provider.tsx
@@ -1,14 +1,13 @@
 'use client'
 
+import {makeStore, type AppStore} from '@/store'
 import {ReactNode, useRef} from 'react'
 import {Provider as ReduxProvider} from 'react-redux'
-import {AppStore, makeStore} from './index'
 
-const StoreProvider = ({children}: {children: ReactNode}) => {
+export const StoreProvider = ({children}: {children: ReactNode}) => {
 	const storeRef = useRef<AppStore>()
 	if (!storeRef.current) storeRef.current = makeStore()
 
 	return <ReduxProvider store={storeRef.current}>{children}</ReduxProvider>
 }
 
-export default StoreProvider
