16ae28fc31f9050c227f7f67467ba3b6b45a9cc4
glebfarmon <glebfarmon@gmail.com>
2024-05-09 01:40:59 +0200

    Improved skeleton

--- src/modules/exercises/modals/info/skeleton.tsx
+++ src/modules/exercises/modals/info/skeleton.tsx
@@ -1,3 +1,22 @@
+import {DialogFooter, DialogHeader} from '@/components/ui/dialog'
+import {Skeleton} from '@/components/ui/skeleton'
+
 export const InfoModalSkeleton = () => {
-	return null
+	return (
+		<>
+			<DialogHeader>
+				<Skeleton className={'m-auto h-[25px] w-[120px] sm:m-0'} />
+				<Skeleton className={'h-[25px] w-[100px]'} />
+			</DialogHeader>
+			<div className={'grid gap-4 py-4'}>
+				<Skeleton className={'h-[100px] w-full'} />
+				<hr />
+				<Skeleton className={'h-[100px] w-full'} />
+				<hr />
+			</div>
+			<DialogFooter>
+				<Skeleton className={'h-[40px] w-[80px]'} />
+			</DialogFooter>
+		</>
+	)
 }
